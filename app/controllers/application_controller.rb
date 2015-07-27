93e5a02e4dbe287fd0f7831cf6c33609e91eac82
Marcus <marcuslistanng@gmail.com>
2015-07-27 17:38:40 +0000

    fixed appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,15 @@ class ApplicationController < Sinatra::Base
   
   set :views, "app/views"
   set :public, "public"
-
   
   get "/" do
     erb :home
-
- 
-get "/" do
-    redirect to ("/sign_up")
   end
+  
   get "/sign_up" do 
     erb :sign_up
   end
+  
   post "/sign_up" do
     @user= User.new(:full_name => params[:full_name], :email => params[:email], :address => params[:address], :age => params[:age], :phone_number => params[:phone_number], :credit_card_number => params[:credit_card_number])
     @user.save
@@ -36,9 +33,4 @@ post "/donatiom" do
     @user.save
     erb :user
   end
- 
-
-
-
-
 end
